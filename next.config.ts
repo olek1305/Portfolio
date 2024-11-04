85a5f169e58a9aa65353dd4db7bcd9a6bf1b2c5b
olek1305 <olek1305@gmail.com>
2024-11-04 20:04:28 +0100

    downgrade react because of markdown, add markdown

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   reactStrictMode: true,
 };
 
-export default nextConfig;
+const withMDX = createMDX();
+
+export default withMDX(nextConfig);
