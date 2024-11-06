1f5feede8b5b6d2e00bc7c2c694b965b2b4165d2
olek1305 <olek1305@gmail.com>
2024-11-06 21:30:31 +0100

    add setTimeout dino

--- src/pages/components/DinoAnimation.tsx
+++ src/pages/components/DinoAnimation.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function DinoAnimation() {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimate(true);
+    }, 30000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="dino-animation">
       <div className="ground"></div>
-      <img src="/dino.png" alt="Dino" className="dino" />
+      <img
+        src="/dino.png"
+        alt="Dino"
+        className={`dino ${animate ? "animate-run" : ""}`}
+      />
     </div>
   );
 }
