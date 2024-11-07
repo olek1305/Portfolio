1bda43b5f6b043eeba77205031fa7bf61de420ee
olek1305 <olek1305@gmail.com>
2024-11-07 13:58:20 +0100

    fix resolution responsive, add button show/hide dino

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,7 @@ const components = {
 
 export default function Home() {
   const [MdxComponent, setMdxComponent] = useState<React.FC | null>(null);
+  const [showDino, setShowDino] = useState(true);
 
   const handleSelectMarkdown = async (
     category: string,
@@ -38,9 +39,8 @@ export default function Home() {
     const markdownFile =
       category === "home"
         ? `${name.toLowerCase().replace(/\s+/g, "-")}.mdx`
-        : `${category}/${
-            fileName || name.toLowerCase().replace(/\s+/g, "-")
-          }.mdx`;
+        : `${category}/${fileName || name.toLowerCase().replace(/\s+/g, "-")
+        }.mdx`;
 
     try {
       const MdxModule = await import(`./data/markdown/${markdownFile}`);
@@ -62,118 +62,127 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="min-h-screen text-gray-300 p-4 sp-body text-base/5 ">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
-        {/* Sidebar */}
-        <div className="col-span-1 space-y-4 flex flex-col text-center">
-          <div
-            className="sp-container p-4 rounded cursor-pointer"
-            onClick={() => handleSelectMarkdown("home", "home")}
+    <div className="min-h-screen bg-[#0d1117] text-gray-300 p-4 scrollbar-fixed">
+      {/* Centered Wrapper */}
+      <div className="max-w-[1920px] mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
+          {/* Sidebar */}
+          <div className="col-span-1 space-y-4 flex flex-col text-center">
+            <div
+              className="sp-container p-4 rounded cursor-pointer"
+              onClick={() => handleSelectMarkdown("home", "home")}
+            >
+              <h3 className="sp-title">profile</h3>
+              <h2 className="text-lg md:text-xl font-bold text-white">
+                Aleksander Żak
+              </h2>
+              <p className="text-sm md:text-base">
+                <span className="text-blue-300">PHP</span> developer. Poland,
+                Bydgoszcz.
+              </p>
+            </div>
+
+            {/* Experience Section */}
+            <div className="sp-container relative">
+              <h3 className="sp-title">
+                experience ({data.experience.length} total)
+              </h3>
+              <div className="sp-content">
+                <ul className="space-y-2">
+                  {data.experience.map((exp, idx) => (
+                    <Experience
+                      key={idx}
+                      experience={exp}
+                      onSelect={() =>
+                        handleSelectMarkdown(
+                          "experiences",
+                          exp.title,
+                          exp.fileName
+                        )
+                      }
+                    />
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* Projects Section */}
+            <div className="sp-container relative">
+              <h3 className="sp-title">
+                projects ({data.projects.length} total)
+              </h3>
+              <div className="sp-content">
+                <ul className="space-y-2">
+                  {data.projects.map((project, idx) => (
+                    <Project
+                      key={idx}
+                      project={project}
+                      onSelect={() =>
+                        handleSelectMarkdown("projects", project.name)
+                      }
+                    />
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* Skills Section */}
+            <div className="sp-container relative">
+              <h3 className="sp-title">skills ({data.skills.length} total)</h3>
+              <div className="sp-content">
+                <ul className="grid grid-cols-2 gap-2">
+                  {data.skills.map((skill, idx) => (
+                    <Skill
+                      key={idx}
+                      skill={skill}
+                      onSelect={() =>
+                        handleSelectMarkdown("skills", skill.name, skill.fileName)
+                      }
+                    />
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          {/* Main Content */}
+          <div className="sp-container col-span-1 md:col-span-3 p-6">
+            <h3 className="sp-title">Main</h3>
+            {MdxComponent ? (
+              <MDXProvider components={components}>
+                <MdxComponent />
+              </MDXProvider>
+            ) : (
+              <div className="text-center">Click on an item to load content.</div>
+            )}
+            {showDino && <DinoAnimation />}
+          </div>
+        </div>
+        <div className="flex justify-end space-x-4 mt-4">
+        <button
+            onClick={() => setShowDino((prev) => !prev)} // Toggle Dino visibility
+            className="text-purple-400 hover:underline"
           >
-            <h3 className="sp-title">profile</h3>
-            <h2 className="text-lg md:text-xl font-bold text-white">
-              Aleksander Żak
-            </h2>
-            <p className="text-sm md:text-base">
-              <span className="text-blue-300">PHP</span> developer. Poland,
-              Bydgoszcz.
-            </p>
-          </div>
+            {showDino ? "Hide Dino" : "Show Dino"}
+          </button>
+          <a
+            href="/cv/aleksander-zak.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-purple-400 hover:underline"
+          >
+            Resume
+          </a>
 
-          {/* Experience Section */}
-          <div className="sp-container relative">
-            <h3 className="sp-title">
-              experience ({data.experience.length} total)
-            </h3>
-            <div className="sp-content">
-              <ul className="space-y-2">
-                {data.experience.map((exp, idx) => (
-                  <Experience
-                    key={idx}
-                    experience={exp}
-                    onSelect={() =>
-                      handleSelectMarkdown(
-                        "experiences",
-                        exp.title,
-                        exp.fileName
-                      )
-                    }
-                  />
-                ))}
-              </ul>
-            </div>
-          </div>
-
-          {/* Projects Section */}
-          <div className="sp-container relative">
-            <h3 className="sp-title">
-              projects ({data.projects.length} total)
-            </h3>
-            <div className="sp-content">
-              <ul className="space-y-2">
-                {data.projects.map((project, idx) => (
-                  <Project
-                    key={idx}
-                    project={project}
-                    onSelect={() =>
-                      handleSelectMarkdown("projects", project.name)
-                    }
-                  />
-                ))}
-              </ul>
-            </div>
-          </div>
-
-          {/* Skills Section */}
-          <div className="sp-container relative">
-            <h3 className="sp-title">skills ({data.skills.length} total)</h3>
-            <div className="sp-content">
-              <ul className="grid grid-cols-2 gap-2">
-                {data.skills.map((skill, idx) => (
-                  <Skill
-                    key={idx}
-                    skill={skill}
-                    onSelect={() =>
-                      handleSelectMarkdown("skills", skill.name, skill.fileName)
-                    }
-                  />
-                ))}
-              </ul>
-            </div>
-          </div>
+          <a
+            href="https://github.com/olek1305"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-purple-400 hover:underline"
+          >
+            GitHub
+          </a>
         </div>
-
-        {/* Main Content */}
-        <div className="sp-container col-span-1 md:col-span-3 p-6">
-          <h3 className="sp-title">Main</h3>
-          {MdxComponent ? (
-            <MDXProvider components={components}>
-              <MdxComponent />
-            </MDXProvider>
-          ) : (
-            <div className="text-center">Click on an item to load content.</div>
-          )}
-          <DinoAnimation />
-        </div>
-      </div>
-      <div className="flex justify-end space-x-4">
-        <a
-          href="/cv/aleksander-zak.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-purple-400 hover:underline"
-        >
-          Resume
-        </a>
-
-        <a
-          href="https://github.com/olek1305"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-purple-400 hover:underline"
-        >
-          GitHub
-        </a>
       </div>
     </div>
   );
